ba6d111f9ca1e3812ab47d6b5a085e34795c7e2a
Khaleb Lisboa <khaleb.lisboa@gmail.com>
2024-10-17 21:24:18 -0400

    add: test async image compose

--- app/src/main/java/com/example/pokedexcompose/MainActivity.kt
+++ app/src/main/java/com/example/pokedexcompose/MainActivity.kt
@@ -4,13 +4,23 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import coil.compose.AsyncImage
 import com.example.pokedexcompose.ui.theme.PokedexComposeTheme
 
 class MainActivity : ComponentActivity() {
@@ -38,10 +48,30 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
     )
 }
 
+@Composable
+fun MainListItem(modifier: Modifier) {
+    Row(horizontalArrangement = Arrangement.Center) {
+        AsyncImage(
+            model = "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/35.png",
+            contentDescription = null,
+            modifier = modifier
+                .size(80.dp)
+                .clip(CircleShape)
+        )
+        Text(
+            text = "Pokemon",
+            textAlign = TextAlign.Center,
+            modifier = modifier
+                .fillMaxWidth()
+                .align(Alignment.CenterVertically)
+        )
+    }
+}
+
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     PokedexComposeTheme {
-        Greeting("Android")
+        MainListItem(modifier = Modifier)
     }
 }
