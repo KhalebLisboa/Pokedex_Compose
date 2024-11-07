5b1c026c9de029fb0a1dfb940183070f677807f8
Khaleb Lisboa <khaleb.lisboa@gmail.com>
2024-11-07 19:35:20 -0400

    refac: use box to create list item

--- app/src/main/java/com/example/pokedexcompose/MainActivity.kt
+++ app/src/main/java/com/example/pokedexcompose/MainActivity.kt
@@ -2,12 +2,9 @@ package com.example.pokedexcompose
 
 import android.annotation.SuppressLint
 import android.os.Bundle
-import android.util.Log
-import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
@@ -15,48 +12,36 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.aspectRatio
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.lazy.grid.items
 import androidx.compose.foundation.lazy.grid.itemsIndexed
-import androidx.compose.foundation.lazy.items
-import androidx.compose.foundation.lazy.itemsIndexed
-import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Card
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.livedata.observeAsState
-import androidx.compose.runtime.mutableIntStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.alpha
 import androidx.compose.ui.draw.clip
-import androidx.compose.ui.draw.clipToBounds
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.TextStyle
-import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.compose.ui.zIndex
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import coil.compose.AsyncImage
@@ -103,40 +88,39 @@ fun MainList(viewModel: MainViewModel = hiltViewModel()) {
 
 @Composable
 fun MainListItem(modifier: Modifier = Modifier, name: String, index: Int, onClick: () -> Unit) {
-    Card(
-        border = BorderStroke(width = 4.dp, color = Color.Transparent), modifier = modifier.padding(10.dp).aspectRatio(3f/2f),
+    Box(
+        modifier = modifier
+            .padding(10.dp)
+            .clip(RoundedCornerShape(16.dp))
+            .background(Color.Cyan)
+            .fillMaxSize()
+            .clickable(onClick = onClick)
+            .aspectRatio(3f / 2f),
     ) {
-        Row(modifier = Modifier
-            .fillMaxSize()) {
             Column(
+                horizontalAlignment = Alignment.Start,
                 modifier = Modifier
-                    .clickable(onClick = onClick)
-                    .padding(horizontal = 10.dp)
-                    .align(Alignment.CenterVertically),
-                verticalArrangement = Arrangement.SpaceAround,
+                    .padding(start = 10.dp, top = 8.dp, bottom = 5.dp)
+                    .fillMaxHeight()
+                    .zIndex(2f),
+                verticalArrangement = Arrangement.SpaceEvenly
             ) {
                 Text(
                     text = name,
                     style = TextStyle(
-                        fontSize = 10.sp,
+                        fontSize = 20.sp,
                         fontWeight = FontWeight.Bold,
                     ),
-                    modifier = Modifier.padding(horizontal = 10.dp)
-                )
-                Text(
-                    text = name,
-                    modifier = Modifier
                 )
 
-                Text(
-                    text = name,
-                    modifier = Modifier
-                )
+                TextPokemonType(name)
+                TextPokemonType(name)
+
             }
-            Box(modifier = Modifier
-                .fillMaxSize()
-                .clipToBounds()) {
-
+            Box(
+                modifier = Modifier
+                    .align(Alignment.BottomEnd)
+            ) {
                 Image(
                     painter = painterResource(R.drawable.img_pokeball),
                     contentDescription = null,
@@ -146,18 +130,33 @@ fun MainListItem(modifier: Modifier = Modifier, name: String, index: Int, onClic
                         .offset(x = 10.dp, y = 15.dp)
                         .alpha(.6f)
                 )
-
-
                 AsyncImage(
                     model = "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/$index.png",
                     contentDescription = null,
                     modifier = Modifier
-                        .size(90.dp)
-                        .clip(CircleShape)
+                        .size(100.dp)
                         .align(Alignment.BottomEnd)
                 )
             }
-        }
+    }
+}
+
+@Composable
+fun TextPokemonType(name: String){
+    Card(
+        modifier = Modifier.background(Color.Transparent)
+    ) {
+        Text(
+            text = name,
+            style = TextStyle(
+                fontSize = 10.sp,
+                fontWeight = FontWeight.Bold,
+                textAlign = TextAlign.Center
+            ),
+            modifier = Modifier
+                .background(Color.Red)
+                .padding(5.dp)
+        )
     }
 }
 
