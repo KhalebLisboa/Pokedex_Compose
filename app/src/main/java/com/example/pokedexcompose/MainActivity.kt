9d2f4146a9859864a8aebf01dbb3f1fd5bbdc7a6
Khaleb Lisboa <khaleb.lisboa@gmail.com>
2024-11-07 00:20:41 -0400

    refac: list icon pokedex

--- app/src/main/java/com/example/pokedexcompose/MainActivity.kt
+++ app/src/main/java/com/example/pokedexcompose/MainActivity.kt
@@ -7,17 +7,31 @@ import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.offset
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.lazy.grid.items
+import androidx.compose.foundation.lazy.grid.itemsIndexed
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.Card
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -30,12 +44,19 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.alpha
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.clipToBounds
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import coil.compose.AsyncImage
@@ -51,7 +72,7 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             PokedexComposeTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()){
+                Scaffold(modifier = Modifier.fillMaxSize()) {
                     MainList()
                 }
             }
@@ -60,54 +81,83 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Composable
-fun MainList(viewModel: MainViewModel = hiltViewModel()){
+fun MainList(viewModel: MainViewModel = hiltViewModel()) {
 
     val items by viewModel.allPokemon.collectAsStateWithLifecycle()
-    val size by remember{ mutableIntStateOf(items.size) }
-    val context = LocalContext.current
-    Log.e("List size", items.size.toString())
 
     LaunchedEffect(Unit) {
         viewModel.fetchAll(0)
     }
 
-    LazyColumn {
-        itemsIndexed(items){ position, pokemon ->
+    LazyVerticalGrid(
+        columns = GridCells.Fixed(2)
+    ) {
+        itemsIndexed(items) { position, pokemon ->
             MainListItem(name = pokemon.name, index = position + 1, onClick = {
                 viewModel.updateOffset()
                 viewModel.fetchAll()
-
             })
         }
     }
 }
 
 @Composable
-fun MainListItem(modifier: Modifier = Modifier, name: String, index : Int, onClick : () -> Unit) {
-    Row(horizontalArrangement = Arrangement.Center, modifier = Modifier.clickable(onClick = onClick)) {
-        AsyncImage(
-            model = "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/$index.png",
-            contentDescription = null,
-            modifier = modifier
-                .size(80.dp)
-                .clip(CircleShape)
-                .background(Color.Red)
-        )
-        Text(
-            text = name,
-            textAlign = TextAlign.Center,
-            modifier = modifier
-                .fillMaxWidth()
-                .align(Alignment.CenterVertically)
-        )
+fun MainListItem(modifier: Modifier = Modifier, name: String, index: Int, onClick: () -> Unit) {
+    Card(
+        border = BorderStroke(width = 4.dp, color = Color.Transparent), modifier = modifier.padding(10.dp).aspectRatio(3f/2f),
+    ) {
+        Row(modifier = Modifier
+            .fillMaxSize()) {
+            Column(
+                modifier = Modifier
+                    .clickable(onClick = onClick)
+                    .padding(horizontal = 10.dp)
+                    .align(Alignment.CenterVertically),
+                verticalArrangement = Arrangement.SpaceAround,
+            ) {
+                Text(
+                    text = name,
+                    style = TextStyle(
+                        fontSize = 10.sp,
+                        fontWeight = FontWeight.Bold,
+                    ),
+                    modifier = Modifier.padding(horizontal = 10.dp)
+                )
+                Text(
+                    text = name,
+                    modifier = Modifier
+                )
+
+                Text(
+                    text = name,
+                    modifier = Modifier
+                )
+            }
+            Box(modifier = Modifier
+                .fillMaxSize()
+                .clipToBounds()) {
+
+                Image(
+                    painter = painterResource(R.drawable.img_pokeball),
+                    contentDescription = null,
+                    modifier = Modifier
+                        .size(90.dp)
+                        .align(Alignment.BottomEnd)
+                        .offset(x = 10.dp, y = 15.dp)
+                        .alpha(.6f)
+                )
+
+
+                AsyncImage(
+                    model = "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/$index.png",
+                    contentDescription = null,
+                    modifier = Modifier
+                        .size(90.dp)
+                        .clip(CircleShape)
+                        .align(Alignment.BottomEnd)
+                )
+            }
+        }
     }
 }
 
@@ -115,6 +165,17 @@ fun MainListItem(modifier: Modifier = Modifier, name: String, index : Int, onCli
 @Composable
 fun GreetingPreview() {
     PokedexComposeTheme {
-//        MainListItem(modifier = Modifier, name = "pokemon")
+        val fakeList = listOf("", "", "", "")
+
+        LazyVerticalGrid(
+            columns = GridCells.Fixed(2),
+            verticalArrangement = Arrangement.spacedBy(20.dp),
+            horizontalArrangement = Arrangement.spacedBy(10.dp),
+            contentPadding = PaddingValues(horizontal = 10.dp)
+        ) {
+            items(fakeList) {
+                MainListItem(modifier = Modifier, name = "pokemon", index = 1, onClick = {})
+            }
+        }
     }
 }
